a4a0f49869ce9f77417efcf43325cc6e5f05ae63
devcarlosperez <carlosperezsantana04@gmail.com>
2025-03-20 18:33:31 +0000

    initial project

---
+++ Codigo_7.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Codigo_7 extends JFrame {
+  public Codigo_7() {
+    initComponents();
+  }
+
+  public void initComponents() {
+
+  }
+
+  public static void main(String[] args) {
+    new Codigo_7();
+  }
+}
